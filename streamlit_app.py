0364c1ec10bbd592676b3b88c59f2ee850433da4
Vanessa Salinas <v.s.salinas@gmail.com>
2024-08-12 23:47:55 +0000

    grafico circular

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,29 @@ st.write(df_totales)
 # Mostrar DataFrame 3
 st.subheader('Datos del Archivo CSV 3')
 st.write(df_localidades)
+
+# Agregar un selector de provincias
+provincia = st.selectbox('Selecciona una provincia', df_localidades['Provincia'].unique())
+# Filtrar los datos por provincia
+df_filtrado = df_localidades[df_localidades['Provincia'] == provincia]
+# Agrupar los datos por tecnología y sumar los valores
+totales_por_tecnologia = df_filtrado[['ADSL', 'CABLEMODEM', 'DIAL UP', 'FIBRA OPTICA',
+                                    'SATELITAL', 'WIMAX', 'WIRELESS', 'Otras_Tecnologias']].sum()
+# Definir umbral (ajusta según tus necesidades)
+umbral = 1
+# Obtener las categorías con un porcentaje mayor al umbral
+categorias_significativas = totales_por_tecnologia[totales_por_tecnologia > totales_por_tecnologia.sum() * umbral / 100].index
+# Crear la categoría "Otros"
+otros = totales_por_tecnologia[~totales_por_tecnologia.index.isin(categorias_significativas)].sum()
+totales_por_tecnologia['Otros'] = otros
+# Eliminar las categorías menores del DataFrame
+totales_por_tecnologia = totales_por_tecnologia[categorias_significativas.tolist() + ['Otros']]
+# Creando el gráfico circular
+fig, ax = plt.subplots()
+# Crear el gráfico circular con colores aleatorios y etiquetas
+ax.pie(totales_por_tecnologia, labels=totales_por_tecnologia.index, autopct='%1.1f%%', colors=plt.cm.Paired(range(len(totales_por_tecnologia))), startangle=140)
+ax.axis('equal')
+# Agregar título al gráfico
+plt.title('Distribución de tecnologías de conexión')
+# Mostrar el gráfico en Streamlit
+st.pyplot(fig)
